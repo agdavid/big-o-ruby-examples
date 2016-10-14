fe3319fb78ab75066a766c79565081dacac724dc
agdavid <antonio.g.david@gmail.com>
2016-10-14 13:29:28 -0400

    add O(1) example

--- bigo.rb
+++ bigo.rb
@@ -1,4 +1,14 @@
-# O(n) "linear time"
+# O(1) "constant time" - time to complete is the same as n grows
+
+def return_first_array_element(array)
+    begin_time = Time.now
+    array[0]
+    end_time = Time.now
+    run_time = end_time - begin_time
+    return run_time
+end
+
+# O(n) "linear time" - time to complete rises linearly as n grows
 
 def generate_array(n)
     begin_time = Time.now
